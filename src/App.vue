c510f11c4d711291242ce49fdb3c137349b66c0d
Adeyemi Akinyemi <89089354+AdeDevs@users.noreply.github.com>
2022-12-31 14:43:42 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+/* eslint-disable */
+
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>;
+  <MyHomePage />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import MyHomePage from './components/MyHomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyHomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
